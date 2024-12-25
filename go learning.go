e345d178d81c818c2fe69f7a783ca6424d2a4e18
SBHHX <1573057383@qq.com>
2024-12-25 20:33:22 +0800

    重新开始学习go简单提交一下

--- go learning.go
+++ go learning.go
@@ -2,8 +2,25 @@ package main
 
 import "fmt"
 
+// main函数，go语言启动的入口
 func main() {
-	fmt.Println("Hello, World!")
-}
+	//fmt.Println  打印一句话 执行完毕后换行
+	fmt.Println("Hello Golang")
 
-//笑死了，说着又错，我看看先
+	//定义变量
+	var name string = "陈英豪" //go中定义的变量必须使用
+	fmt.Println(name)
+	//string默认值为空
+	//int的默认值为0
+
+	//简单定义
+	num := 18 //编译器自动推导数据类型
+	fmt.Println(num)
+
+	//一次定义多个
+	var (
+		a int
+		b string
+	)
+	fmt.Println(a, b)
+}
